bc16bef538d1c3669fb39c08d7343333192b4175
ATSzimmat <anton.szimmat@campus.lmu.de>
2025-05-31 13:38:23 +0200

    add a prevoious script

---
+++ Script with Git.R
@@ -0,0 +1,10 @@
+# Now we can do something
+
+library(tidyverse)
+
+data_for_git <-mtcars
+
+data_for_git
+
+data_for_git %>% ggplot( aes(x = cyl, y= hp, color = factor(vs))) + geom_smooth(method ="lm")
+
