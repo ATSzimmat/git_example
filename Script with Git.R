70c280c14dc1e0efa21678c980f34820126a7c21
ATSzimmat <anton.szimmat@campus.lmu.de>
2025-05-31 13:53:15 +0200

    adding a new theme

--- Script with Git.R
+++ Script with Git.R
@@ -6,5 +6,6 @@ data_for_git <-mtcars
 
 data_for_git
 
-data_for_git %>% ggplot( aes(x = cyl, y= hp, color = factor(vs))) + geom_smooth(method ="lm")
+data_for_git %>% ggplot( aes(x = cyl, y= hp, color = factor(vs))) + geom_smooth(method ="lm") + theme_bw()
+
 
