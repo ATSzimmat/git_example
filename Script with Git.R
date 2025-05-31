6dc06aaac17cb1ffa7dd50904b24ce5ed4e64f70
ATSzimmat <anton.szimmat@campus.lmu.de>
2025-05-31 14:42:29 +0200

    added a calculation

--- Script with Git.R
+++ Script with Git.R
@@ -8,4 +8,5 @@ data_for_git
 
 data_for_git %>% ggplot( aes(x = cyl, y= hp, color = factor(vs))) + geom_smooth(method ="lm") + theme_bw()
 
-
+# Now somebody else adds a calculation
+12454574567 +659468794567
